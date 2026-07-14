014990ccd73df59ab453e227f81120b59e981518
Holly Cummins <holly.k.cummins@googlemail.com>
2026-07-14 20:40:11 +0100

    Tidying

--- migration/src/main/java/io/quarkus/tools/JekyllPluginConverter.java
+++ migration/src/main/java/io/quarkus/tools/JekyllPluginConverter.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class JekyllPluginConverter {
